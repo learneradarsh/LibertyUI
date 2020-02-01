9a6e0ded9cb0a0a7fd36fa0facc98822283fd129
learneradarsh <learner.adarsh@gmail.com>
2020-02-01 15:21:42 +0530

    ADDED: SCRIPTS FOR CURRENT CARD

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,9 +57,9 @@
           options: options
         });
       }
-      if ($('#sales-chart').length) {
+      if ($('#morris-line-example').length) {
         Morris.Line({
-          element: 'sales-chart',
+          element: 'morris-line-example',
           lineColors: ['rgba(70, 77, 228, 0.8)', 'rgba(217, 225 ,253, 1)'],
           data: [{
               y: '2006',
@@ -97,6 +97,66 @@
           ykeys: ['a', 'b'],
           labels: ['Series A', 'Series B'],
           hideHover: "always"
+        });
+      }
+      if ($("#current-chart").length) {
+        var CurrentChartCanvas = $("#current-chart").get(0).getContext("2d");
+        var CurrentChart = new Chart(CurrentChartCanvas, {
+          type: 'bar',
+          data: {
+            labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+            datasets: [{
+                label: 'Profit',
+                data: [330, 380, 230, 400, 309, 530, 340, 200],
+                backgroundColor: 'rgba(70, 77, 228, 1)'
+              },
+              {
+                label: 'Target',
+                data: [600, 600, 600, 600, 600, 600, 600],
+                backgroundColor: 'rgba(238, 242, 245, 1)'
+              }
+            ]
+          },
+          options: {
+            responsive: true,
+            maintainAspectRatio: true,
+            layout: {
+              padding: {
+                left: 0,
+                right: 0,
+                top: 20,
+                bottom: 0
+              }
+            },
+            scales: {
+              yAxes: [{
+                display: false,
+                gridLines: {
+                  display: false
+                }
+              }],
+              xAxes: [{
+                stacked: true,
+                ticks: {
+                  beginAtZero: true,
+                  fontColor: "#354168"
+                },
+                gridLines: {
+                  color: "rgba(0, 0, 0, 0)",
+                  display: false
+                },
+                barPercentage: 0.4
+              }]
+            },
+            legend: {
+              display: false
+            },
+            elements: {
+              point: {
+                radius: 0
+              }
+            }
+          }
         });
       }
     });
